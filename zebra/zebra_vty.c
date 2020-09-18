bddb73e43edb4d5130f878daed42210766a50494
Donald Sharp <sharpd@nvidia.com>
2020-09-18 15:41:19 -0400

    zebra: Add basic knowledge of asic offload available
    Some linux kernels are starting to support the idea of knowledge
    about the underlying asic.  Add a boolean that we can set/unset
    to track whether or not we think the router has this functionality
    available.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -3453,6 +3453,9 @@ DEFUN (show_zebra,
 {
 	struct vrf *vrf;
 
+	if (zrouter.asic_offloaded)
+		vty_out(vty, "Asic Offload is being used\n");
+
 	vty_out(vty,
 		"                            Route      Route      Neighbor   LSP        LSP\n");
 	vty_out(vty,
